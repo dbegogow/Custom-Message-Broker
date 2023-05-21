6ee5798b385e6d86d76c3d05c4c9917df4d4b6b7
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-21 13:04:29 +0300

    Create MessageReadModel

---
+++ Subscriber/Models/MessageReadModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Subscriber.Models;
+
+public class MessageReadModel
+{
+    public int Id { get; init; }
+
+    public string TopicMessage { get; init; }
+
+    public DateTime ExpiresAfter { get; init; }
+
+    public string MessageStatus { get; init; }
+}
